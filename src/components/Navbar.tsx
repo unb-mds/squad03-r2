a9208d565d44e137e3c93d9c865110a50c27563f
Iago Passaglia <iago210669@gmail.com>
2023-12-05 19:49:01 -0300

    Correção de responsividade - NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar({ activeButton }: NavbarProps) {
     <header className="bg-[#b156c8] w-full h-14 fixed top-0  rounded">
       <div className="container h-full flex items-center justify-center lg:justify-start">
       <img src="https://raw.githubusercontent.com/Paxxaglia/Trabalaho-de-calango-Jogo-de-adivinhacao/main/logo-licital.png" alt="Logo" className="w-[15rem] mt-1 ml-1 hidden md:block" />
-        <ul className="flex ml-[300px] items-center space-x-4 w-full">
+        <ul className="flex items-center space-x-4 w-full items-center md:justify-center">
           <li className="px-4">
             <Link href="/">
               <HomeIcon
