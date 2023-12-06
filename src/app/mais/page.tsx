accf0c983b8b135998fd04141ed2b543e0adec36
Iago Passaglia <iago210669@gmail.com>
2023-12-05 21:28:11 -0300

    Adição de feedback do usuario por meio de envio de email

--- src/app/mais/page.tsx
+++ src/app/mais/page.tsx
@@ -1,9 +1,38 @@
+"use client";
 import Footer from "@/components/Footer";
 import { MainLayout } from "@/layouts/MainLayout";
 import Link from "next/link";
 import OKBRIcon from "@/assets/svgs/okbr-icon";
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Mais() {
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function enviaEmail(e: React.FormEvent<HTMLFormElement>){  
+    e.preventDefault();
+
+    const templateParams = {
+      nome: nome,
+      message: message,
+      email: email
+    }
+    emailjs.send("service_bbza6m4","template_rpknht7", templateParams, "WuPfoC3iextGSpFZy")
+    .then((response) => {
+      console.log("email enviado", response.status, response.text)
+      setEmail('');
+      setMessage('');
+      setNome('');
+
+    }, (error)=>{
+      alert('Não foi possivel enviar o email');
+      console.log(error);
+    })
+  }
+
   return (
     <MainLayout activeButton={"Mais"}>
       <div className="w-full flex flex-col gap-y-3 3xl:gap-y-5 mt-9 flex grow">
@@ -49,18 +78,18 @@ export default function Mais() {
           <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Nos contate!</h2>
             <p className="mb-8 lg:mb-16 font-light text-center dark:text-white sm:text-xl">Tem um problema técnico? Quer enviar alguma sujestão sobre um recurso? Nos informe.</p>
-            <form action="#" className="space-y-8">
-              <div>
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Seu email</label>
-                <input type="email" id="email" className="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="nome@gmail.com" required />
+            <form action="#" className="space-y-8" onSubmit={enviaEmail}>
+            <div>
+                <label htmlFor="nome" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Seu Nome</label>
+                <input onChange={(e) => setNome(e.target.value)} value={nome} type="text" id="nome" className="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="João da Silva Sauro" required />
               </div>
               <div>
-                <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Assunto</label>
-                <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Como podemos te ajudar?" required />
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+                <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id="email" className="shadow-sm bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="seuEmail@gmail.com" required />
               </div>
               <div className="sm:col-span-2">
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sua Mensagem</label>
-                <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Escreva sua mensagem..."></textarea>
+                <textarea onChange={(e) => setMessage(e.target.value)} value={message} id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Escreva sua mensagem..."></textarea>
               </div>
               <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white dark:rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-purple-900 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:border dark:border-purple-900">Enviar Mensagem</button>
             </form>
