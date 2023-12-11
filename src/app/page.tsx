8ad3258b60807f341c0cdd2bb59646236ee26613
Iago Passaglia <iago210669@gmail.com>
2023-12-10 22:33:32 -0300

    Adicionando grafico separado para cada tipo de informação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,18 @@ import TotalAtos from "@/components/charts/TotalAtos";
 import { MainLayout } from "@/layouts/MainLayout";
 import Municipio from "@/components/Municipio";
 import Head from "next/head";
+import TotalLicitacoes from "@/components/charts/TotalLicitacoes";
+import { SetStateAction, useState } from "react";
 
 
 export default function Home() {
+
+  const [selectedOption, setSelectedOption] = useState('Gastos');
+  const handleDropdownChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setSelectedOption(event.target.value);
+  };
+
+
   return (
     <main>
       <Head>
@@ -16,7 +25,15 @@ export default function Home() {
       </Head>
       <MainLayout activeButton={"Home"}>
         <Municipio municipioId={"geral"} backActive={false} ano={"geral"}>
-          <TotalAtos municipio={"geral"} ano={"todos"} />
+          <select value={selectedOption} onChange={handleDropdownChange} className="md:w-[10 rem] h-16 md:p-4 rounded-2xl text-lg mt-3">
+            <option value="Quantidade de Licitações">Quantidade de Licitações</option>
+            <option value="Gastos">Gastos</option>
+          </select>
+          {selectedOption === 'Quantidade de Licitações' ? (
+            <TotalLicitacoes municipio={'geral'} ano={'todos'} />
+          ) : (
+            <TotalAtos municipio={'geral'} ano={'todos'} />
+          )}
           {/* <CidadesNomeacao />
           <CidadesExoneracao /> */}
         </Municipio>
