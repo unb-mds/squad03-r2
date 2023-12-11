1d47efb1292184d20c2712cf8b905a1d50ab77cd
Iago Passaglia <iago210669@gmail.com>
2023-12-10 23:55:46 -0300

    correção de logo - Rodapé

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex flex-col md:flex md:flex-row text-xs text-[#6C6C6C] gap-x-5 items-center gap-y-5">
-      <img className="w-40 md:w-30" src="https://raw.githubusercontent.com/Paxxaglia/Trabalaho-de-calango-Jogo-de-adivinhacao/main/logo-licital.png" />
+      <img className="w-30 md:w-20" src="https://github.com/unb-mds/squad03-r2/blob/main/src/assets/images/logo3.png?raw=true" />
       <p>
         Projeto feito com base no 
         <span className="underline decoration-solid">
