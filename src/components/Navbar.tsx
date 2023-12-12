fbf327065196e3b52f502449b5f2e331566b5e17
Iago Passaglia <iago210669@gmail.com>
2023-12-12 17:43:16 -0300

    reset

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar({ activeButton }: NavbarProps) {
   return (
     <header className="bg-[#b156c8] w-full h-14 fixed top-0  rounded-bt">
       <div className="container h-full flex items-center justify-center lg:justify-start">
-      <img src="https://github.com/unb-mds/2023-2-Squad03-LicitAL/blob/main/logo1.png?raw=true" alt="Logo" className="w-[7rem] mt-1 ml-1 hidden md:block" />
+      <img src="https://github.com/unb-mds/squad03-r2/blob/main/src/assets/images/logo1.png?raw=true" alt="Logo" className="w-[7rem] mt-1 ml-1 hidden md:block" />
         <ul className="flex items-center space-x-4 w-full items-center md:justify-center">
           <li className="px-4">
             <Link href="/">
